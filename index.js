0e2011cbdc1bcdf60659483b60be8b90095011e5
abhay-srivastav <abhaysrivastav007@gmail.com>
2019-06-03 14:35:41 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer({});
+
+http.createServer(function(req, res) {
+	if (req.method === 'OPTIONS') {
+		enableCors(req, res);
+		res.writeHead(200);
+		res.end();
+		return;
+	}
+    proxy.web(req, res, { target: 'https://api.scratch.mit.edu', secure:true,changeOrigin:true });
+}).listen(3000);
+var enableCors = function(req, res) {
+	if (req.headers['access-control-request-method']) {
+		res.setHeader('access-control-allow-methods', req.headers['access-control-request-method']);
+	}
+
+	if (req.headers['access-control-request-headers']) {
+		res.setHeader('access-control-allow-headers', req.headers['access-control-request-headers']);
+	}
+
+	if (req.headers.origin) {
+		res.setHeader('access-control-allow-origin', req.headers.origin);
+		res.setHeader('access-control-allow-credentials', 'true');
+	}
+};
+proxy.on('proxyRes', (proxyRes, req, res) => {
+	enableCors(req, res);
+});
